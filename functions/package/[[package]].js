4cc9d2c5e8de46f7a09d789293bac49f38af0816
Dilip Kr. Shukla <dilip.shukla@tre.se>
2021-12-05 22:00:20 +0100

    improve selection of readme file

--- functions/package/[[package]].js
+++ functions/package/[[package]].js
@@ -2,78 +2,50 @@ import { Helmet, jsx, renderSSR } from "nano-jsx";
 import { marked } from "marked";
 import Package from "../../components/package.js";
 
-async function fetchAndCache(url, waitUntil){
-  const cacheUrl = new URL(url)
+async function onRequestGet({ params, waitUntil, request }) {
+  try {
+    const NPM_PROVIDER_URL = "https://ga.jspm.io/npm:";
+    const packageName = params.package.join("/");
+    const baseURL = `${NPM_PROVIDER_URL}${packageName}`;
 
-  // Construct the cache key from the cache URL
-  const cacheKey = new Request(cacheUrl.toString())
-  const cache = caches.default
+    const filesToFetch = ["package.json", "README.md", "readme.md"];
+    
+    const [jspmPackage, README, readme] = await Promise.all(
+      filesToFetch.map((file) =>
+        fetch(
+          `${baseURL}/${file}`,
+        )
+      ),
+    )
+    
+    const readmeFileContent = await [README, readme].find(readmeFile => readmeFile.status === 200).text();
+    const readmeHTML = marked.parse(readmeFileContent);
 
-  // Check whether the value is already available in the cache
-  // if not, you will need to fetch it from origin, and store it in the cache
-  // for future access
-  let response = await cache.match(cacheKey)
+    const {
+      name,
+      description,
+      keywords,
+      version,
+      homepage,
+      license,
+      files,
+      exports,
+    } = await jspmPackage.json();
 
-  if (!response) {
-    // If not in cache, get it from origin
-    response = await fetch(url)
+    const app = renderSSR(
+      jsx
+        `<${Package} name=${name} description=${description} version=${version} homepage=${homepage} license=${license} files=${files} exports=${exports} readme=${readmeHTML} keywords=${keywords} />`,
+    );
 
-    // Must use Response constructor to inherit all of response's fields
-    response = new Response(response.body, response)
+    const { body, head, footer } = Helmet.SSR(app);
 
-    // Cache API respects Cache-Control headers. Setting s-max-age to 10
-    // will limit the response to be in cache for 10 seconds max
-
-    // Any changes made to the response here will be reflected in the cached value
-    // response.headers.append("Cache-Control", "s-maxage=10")
-
-    // Store the fetched response as cacheKey
-    // Use waitUntil so you can return the response without blocking on
-    // writing to cache
-    waitUntil(cache.put(cacheKey, response.clone()))
-  }
-  return response
-}
-
-async function onRequestGet({ params, waitUntil }) {
-  const NPM_PROVIDER_URL = "https://ga.jspm.io/npm:";
-  const packageName = params.package.join("/");
-
-  const jspmPackage = await fetchAndCache(
-    `${NPM_PROVIDER_URL}${packageName}/package.json`,
-    waitUntil
-  );
-  const {
-    name,
-    description,
-    keywords,
-    version,
-    homepage,
-    license,
-    files,
-    exports,
-  } = await jspmPackage.json();
-  
-  const readmeFileNames = ["readme.md", "README.md"]
-  const readmeFile = await Promise.any(readmeFileNames.map(readmeFileName => fetchAndCache(
-    `${NPM_PROVIDER_URL}${packageName}/${readmeFileName}`,
-    waitUntil
-  )));
-  const readmeFileContent = await readmeFile.text();
-  const readmeHTML = marked.parse(readmeFileContent);
-  const app = renderSSR(
-    jsx
-      `<${Package} name=${name} description=${description} version=${version} homepage=${homepage} license=${license} files=${files} exports=${exports} readme=${readmeHTML} />`,
-  );
-  const { body, head, footer } = Helmet.SSR(app);
-
-  const css = `
+    const css = `
       jspm-package-name, jspm-package-version, jspm-package-description, jspm-package-license, jspm-package-file{
           display: block;
       }
     `;
 
-  const html = `
+    const html = `
     <!DOCTYPE html>
     <html lang="en">
       <head>
@@ -90,9 +62,12 @@ async function onRequestGet({ params, waitUntil }) {
       </body>
     </html>`;
 
-  return new Response(html, {
-    headers: { "content-type": "text/html; charset=UTF-8" },
-  });
+    return new Response(html, {
+      headers: { "content-type": "text/html; charset=UTF-8" },
+    });
+  } catch (error) {
+    return new Response(`${error.message}\n${error.stack}`, { status: 500 });
+  }
 }
 
 export { onRequestGet };
