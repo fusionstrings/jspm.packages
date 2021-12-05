195c27b805b00b3ad5f30f43ab1f1b7ca399d90e
Dilip Kr. Shukla <dilip.shukla@tre.se>
2021-12-06 00:03:22 +0100

    use cache control

--- functions/package/[[package]].js
+++ functions/package/[[package]].js
@@ -2,40 +2,26 @@ import { Helmet, jsx, renderSSR } from "nano-jsx";
 import { marked } from "marked";
 import Package from "../../components/package.js";
 
-async function onRequestGet({ params, env }) {
+async function onRequestGet({ params, env, waitUntil }) {
   try {
     const NPM_PROVIDER_URL = "https://ga.jspm.io/npm:";
     const packageName = params.package.join("/");
     const baseURL = `${NPM_PROVIDER_URL}${packageName}`;
-    const { README_HTML } = env;
-    
-    let readmeHTML = README_HTML ? await README_HTML.get(`${baseURL}/readme.md.html`) : '';
-    let jspmPackage;
+    const filesToFetch = ["package.json", "README.md", "readme.md"];
 
-    if (readmeHTML) {
-      jspmPackage = fetch(
-        `${baseURL}/package.json`,
-      );
-    } else {
-      const filesToFetch = ["package.json", "README.md", "readme.md"];
+    const [jspmPackage, README, readme] = await Promise.all(
+      filesToFetch.map((file) =>
+        fetch(
+          `${baseURL}/${file}`,
+        )
+      ),
+    );
 
-      const [jspmPackageResponse, README, readme] = await Promise.all(
-        filesToFetch.map((file) =>
-          fetch(
-            `${baseURL}/${file}`,
-          )
-        ),
-      );
+    const readmeFileContent = await [README, readme].find((readmeFile) =>
+      readmeFile.status === 200
+    ).text();
 
-      const readmeFileContent = await [README, readme].find((readmeFile) =>
-        readmeFile.status === 200
-      ).text();
-      readmeHTML = marked.parse(readmeFileContent);
-      jspmPackage = jspmPackageResponse;
-      if(README_HTML) {
-        await README_HTML.put(`${baseURL}/readme.md.html`, readmeHTML)
-      }
-    }
+    const readmeHTML = marked.parse(readmeFileContent);
 
     const {
       name,
@@ -65,8 +51,10 @@ async function onRequestGet({ params, env }) {
     <!DOCTYPE html>
     <html lang="en">
       <head>
+        <title>${name}@${version} - JSPM</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content=${description}>
         <style>
           ${css}
         </style>
@@ -79,7 +67,10 @@ async function onRequestGet({ params, env }) {
     </html>`;
 
     return new Response(html, {
-      headers: { "content-type": "text/html; charset=UTF-8" },
+      headers: {
+        "content-type": "text/html; charset=UTF-8",
+        "Cache-Control": "max-age=1500",
+      },
     });
   } catch (error) {
     return new Response(`${error.message}\n${error.stack}`, { status: 500 });
