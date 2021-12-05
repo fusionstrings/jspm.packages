3c58fb04c60c07100e7d221a202c9fa7da10411c
Dilip Kr. Shukla <dilip.shukla@tre.se>
2021-12-05 22:31:14 +0100

    use KV store to improve speed

--- functions/package/[[package]].js
+++ functions/package/[[package]].js
@@ -2,24 +2,40 @@ import { Helmet, jsx, renderSSR } from "nano-jsx";
 import { marked } from "marked";
 import Package from "../../components/package.js";
 
-async function onRequestGet({ params, waitUntil, request }) {
+async function onRequestGet({ params, env }) {
   try {
     const NPM_PROVIDER_URL = "https://ga.jspm.io/npm:";
     const packageName = params.package.join("/");
     const baseURL = `${NPM_PROVIDER_URL}${packageName}`;
+    const { README_HTML } = env;
+    
+    let readmeHTML = README_HTML ? await README_HTML.get(`${baseURL}/readme.md.html`) : '';
+    let jspmPackage;
 
-    const filesToFetch = ["package.json", "README.md", "readme.md"];
-    
-    const [jspmPackage, README, readme] = await Promise.all(
-      filesToFetch.map((file) =>
-        fetch(
-          `${baseURL}/${file}`,
-        )
-      ),
-    )
-    
-    const readmeFileContent = await [README, readme].find(readmeFile => readmeFile.status === 200).text();
-    const readmeHTML = marked.parse(readmeFileContent);
+    if (readmeHTML) {
+      jspmPackage = fetch(
+        `${baseURL}/package.json`,
+      );
+    } else {
+      const filesToFetch = ["package.json", "README.md", "readme.md"];
+
+      const [jspmPackageResponse, README, readme] = await Promise.all(
+        filesToFetch.map((file) =>
+          fetch(
+            `${baseURL}/${file}`,
+          )
+        ),
+      );
+
+      const readmeFileContent = await [README, readme].find((readmeFile) =>
+        readmeFile.status === 200
+      ).text();
+      readmeHTML = marked.parse(readmeFileContent);
+      jspmPackage = jspmPackageResponse;
+      if(README_HTML) {
+        README_HTML.put(`${baseURL}/readme.md.html`, readmeHTML)
+      }
+    }
 
     const {
       name,
